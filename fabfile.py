9233ad138b2fd967674f474f3f0b9ab2f3cdb9e5
Nathan Humphreys <nathan@zenguard.org>
2015-07-28 18:34:48 +0200

    Add template and static deploy to fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,19 +1,30 @@
 from fabric.api import local, env, sudo
 
 env.hosts = ['nkhumphreys.co.uk']
+env.user = 'root'
 
 NAME = "gobananas"
 
 
 def deploy():
-    cmd = "scp {local_path} root@{host}:{remote_path}"
+    base_cmd = "scp -r {local_path} root@{host}:{remote_path}"
 
-    remote_path = "/tmp/golang_blog"
+    remote_path = "/tmp"
+    template_path = "/var/www/templates/"
+    static_path = "/var/www/static/"
 
     for h in env.hosts:
-        cmd = cmd.format(local_path=NAME,
-                         host=h,
-                         remote_path=remote_path)
+        cmd = base_cmd.format(local_path=NAME,
+                              host=h,
+                              remote_path=remote_path)
+        local(cmd)
+        cmd = base_cmd.format(local_path="./templates/*",
+                              host=h,
+                              remote_path=template_path)
+        local(cmd)
+        cmd = base_cmd.format(local_path="./static/*",
+                              host=h,
+                              remote_path=static_path)
         local(cmd)
 
     sudo("mv %s/%s /usr/bin" % (remote_path, NAME))
